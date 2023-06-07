283918951a7889d6bf23835b16b7a507bd909ac2
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-06-07 17:43:04 +0530

    file chnages

--- main.tf
+++ main.tf
@@ -10,8 +10,29 @@ module "subnets" {
   for_each = var.subnets
   vpc_id = aws_vpc.main.id
   cidr_block = each.value["cidr_block"]
+  azs = each.value["azs"]
   env = var.env
   tags = var.tags
   name = each.value["name"]
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tag = merge(var.tag, {Name = "${var.env}-igw"})
+}
+
+resource "aws_eip" "ngw" {
+  count = length(var.subnets["public"].cidr_block)
+  vpc = true
+  tag = merge(var.tag, {Name = "${var.env}-ngw"})
+}
+
+resource "aws_nat_gateway" "ngw" {
+  count = length(var.subnets["public"].cidr_block)
+  allocation_id = aws_eip.ngw[count.index].id
+  subnet_id     = module.subnets["public"].subnet_ids[count.index]
+
+  tag = merge(var.tag, {Name = "${var.env}-ngw"})
 
 }
--- subnets/main.tf
+++ subnets/main.tf
@@ -2,7 +2,13 @@ resource "aws_subnet" "main" {
   count = length(var.cidr_block)
   vpc_id     = var.vpc_id
   cidr_block = var.cidr_block[count.index]
+  avavailability_zone = var.azs[count.index]
+  tags = merge(var.tag, {Name = "${var.env}-${var.name}-subnet-${count.index + 1}"})
 
-  tags = merge(var.tag, {Name = "${var.env}-${var.name}-subnet-${count.index}"})
+}
 
+resource "aws_route_table" "example" {
+  count = length(var.cidr_block)
+  vpc_id = aws_vpc.main.id
+  tags = merge(var.tag, {Name = "${var.env}-${var.name}-rwt-${count.index + 1}"})
 }
