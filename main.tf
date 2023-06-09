5ae85ef8e348d1396ae840f2358347f98b5f8eb6
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-06-09 21:04:09 +0530

    file chnaess

--- main.tf
+++ main.tf
@@ -1,38 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
-  enable_dns_hostnames = true
-  enable_dns_support = true
-  tag = merge(var.tag, {Name = "${var.env}-vpc"})
-}
-
-module "subnets" {
-  source = "./subnets"
-  for_each = var.subnets
-  vpc_id = aws_vpc.main.id
-  cidr_block = each.value["cidr_block"]
-  azs = each.value["azs"]
   env = var.env
-  tags = var.tags
-  name = each.value["name"]
-}
-
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
-
-  tag = merge(var.tag, {Name = "${var.env}-igw"})
-}
-
-resource "aws_eip" "ngw" {
-  count = length(var.subnets["public"].cidr_block)
-  vpc = true
-  tag = merge(var.tag, {Name = "${var.env}-ngw"})
-}
-
-resource "aws_nat_gateway" "ngw" {
-  count = length(var.subnets["public"].cidr_block)
-  allocation_id = aws_eip.ngw[count.index].id
-  subnet_id     = module.subnets["public"].subnet_ids[count.index]
-
-  tag = merge(var.tag, {Name = "${var.env}-ngw"})
-
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tag = merge(var.tag, {Name = "${var.env}-vpc"})
 }
--- subnets/main.tf
+++ subnets/main.tf
@@ -7,8 +7,14 @@ resource "aws_subnet" "main" {
 
 }
 
-resource "aws_route_table" "example" {
+resource "aws_route_table" "main" {
   count = length(var.cidr_block)
   vpc_id = aws_vpc.main.id
   tags = merge(var.tag, {Name = "${var.env}-${var.name}-rwt-${count.index + 1}"})
+}
+
+resource "aws_route_table_association" "route_association" {
+  count = length(var.cidr_block)
+  subnet_id      = aws_subnet.main[count.index].id
+  route_table_id = aws_route_table.main[count.index].id
 }
