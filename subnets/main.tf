22c51532a961d9390a5d846f1fe3c2a238e475b7
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-06-11 11:21:35 +0530

    file chnages

--- subnets/main.tf
+++ subnets/main.tf
@@ -8,6 +8,6 @@ resource "aws_subnet" "main" {
 
 resource "aws_route_table" "main" {
   count = length(var.cidr_block)
-  vpc_id = aws_vpc.main.id
+  vpc_id = var.vpc_id
   tags = merge(var.tags, {Name = "${var.env}-${var.name}-${count.index+1}"})
 }
