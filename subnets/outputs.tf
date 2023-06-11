148e6c051ce1859f664d6347f6940b0fda585364
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-06-11 18:35:24 +0530

    file chnages

--- subnets/outputs.tf
+++ subnets/outputs.tf
@@ -2,9 +2,9 @@ output "subnet_ids" {
   value = aws_subnet.main.*.id
 }
 
-output "subnet_cidr_block" {
-  value = aws_subnet.main.*.block
-}
+//output "subnet_cidr_block" {
+ // value = aws_subnet.main.*.block
+//}
 
 output "route_table_ids" {
   value = aws_route_table.main.*.id
