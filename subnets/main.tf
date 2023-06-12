54d75b03e44fcd80d02bbaf2af104769ac097e54
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-06-12 17:21:53 +0530

    file changes

--- subnets/main.tf
+++ subnets/main.tf
@@ -17,7 +17,3 @@ resource "aws_route_table_association" "association" {
   subnet_id      = aws_subnet.main[count.index].id
   route_table_id = aws_route_table.main[count.index].id
 }
-
-output "subnet_cidrs" {
-  value = aws_subnet.main.*.id
-}
